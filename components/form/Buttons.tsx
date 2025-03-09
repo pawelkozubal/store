42a9595b6c0278aa59c5f62566c096255d2f87b1
pawelkozubal <31888831+pawelkozubal@users.noreply.github.com>
2025-03-09 00:39:29 +0000

    fixing symbol icon

--- components/form/Buttons.tsx
+++ components/form/Buttons.tsx
@@ -1,12 +1,14 @@
 'use client';
 
-import { ReloadIcon } from '@radix-ui/react-icons';
+import { SymbolIcon } from '@radix-ui/react-icons';
 import { useFormStatus } from 'react-dom';
 import { Button } from '@/components/ui/button';
 import { cn } from '@/lib/utils';
 import { SignInButton } from '@clerk/nextjs';
 import { FaRegHeart, FaHeart } from 'react-icons/fa';
-import { LuTrash2, LuSquare } from 'react-icons/lu';
+import { LuTrash2, LuSquare } from 'react-icons/lu'; 
+
+
 
 type btnSize = 'default' | 'lg' | 'sm';
 
@@ -21,7 +23,7 @@ export function SubmitButton({className = '', text = 'submit', size = 'lg',}:Sub
     const {pending} = useFormStatus()
     return <Button type='submit' disabled = { pending} className={cn('capitalize',className)} size={size}>
         {pending ? <>
-        <ReloadIcon className='mr-2 h-4 w-4 animate-spin'/>
+        <SymbolIcon className='mr-2 h-4 w-4 animate-spin'/>
         Please wait...
         </>:text }
     </Button>
