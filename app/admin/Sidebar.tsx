a196497d920193a6d328b14939247aa017487720
pawelkozubal <31888831+pawelkozubal@users.noreply.github.com>
2025-03-15 02:37:59 +0000

    new changes

--- app/admin/Sidebar.tsx
+++ app/admin/Sidebar.tsx
@@ -16,9 +16,10 @@ function Sidebar() {
           <Button
             asChild
             className='w-full mb-2 capitalize font-normal justify-start'
-            variant={variant}
+            variant={variant} 
+            key={link.href}
           >
-            <Link key={link.href} href={link.href}>
+            <Link href={link.href}>
               {link.label}
             </Link>
           </Button>
