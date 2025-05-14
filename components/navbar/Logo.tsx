9b4a1005fb21c2cfb86c95e557cff4263a903db8
pawelkozubal <31888831+pawelkozubal@users.noreply.github.com>
2025-05-14 23:00:27 +0100

    added about section

--- components/navbar/Logo.tsx
+++ components/navbar/Logo.tsx
@@ -6,7 +6,8 @@ function Logo() {
   return (
     <Button size='icon' asChild>
       <Link href='/'>
-        Haven & Hue <VscCode className='w-6 h-6' />
+        <h1 className='flex flex-wrap gap-2 sm:gap-x-6 items-center justify-center text-4xl font-bold leading-none tracking-wide sm:text-6xl'>Haven & Hue</h1>
+        <VscCode className='w-6 h-6' />
       </Link>
     </Button>
   );
