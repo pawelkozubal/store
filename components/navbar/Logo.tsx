c356e9898ca278125d137419675b458abd784953
pawelkozubal <31888831+pawelkozubal@users.noreply.github.com>
2025-05-14 23:03:50 +0100

    changing the logo

--- components/navbar/Logo.tsx
+++ components/navbar/Logo.tsx
@@ -6,7 +6,6 @@ function Logo() {
   return (
     <Button size='icon' asChild>
       <Link href='/'>
-        <h1 className='flex flex-wrap gap-2 sm:gap-x-6 items-center justify-center text-4xl font-bold leading-none tracking-wide sm:text-6xl'>Haven & Hue</h1>
         <VscCode className='w-6 h-6' />
       </Link>
     </Button>
