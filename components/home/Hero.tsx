50e4b5b1cdf5a4ddee37e7fa33652cc6add297f2
pawelkozubal <31888831+pawelkozubal@users.noreply.github.com>
2025-05-16 22:54:40 +0100

    changed to the main page

--- components/home/Hero.tsx
+++ components/home/Hero.tsx
@@ -6,9 +6,12 @@ function Hero() {
   return (
     <section className='grid grid-cols-1 lg:grid-cols-2 gap-24 items-center'>
       <div>
-        <h1 className='max-w-2xl font-bold text-4xl tracking-tight sm:text-6xl'>
-          Where Comfort Meets Character
+        <h1 className='max-w-2xl font-bold text-4xl tracking-tight sm:text-6xl text-primary-foreground'>
+          Heaven & Hue
         </h1>
+        <h3 className='max-w-2xl font-semibold text-4xl tracking-tight sm:text-6xl'>
+          Where Comfort Meets Character
+        </h3>
         <p className='mt-8 max-w-xl text-lg leading-8 text-muted-foreground'>
           Discover thoughtfully designed pieces that turn your space into a sanctuary. From cozy beds to statement lighting and stylish accents,
           Haven & Hue brings comfort and personality home.
