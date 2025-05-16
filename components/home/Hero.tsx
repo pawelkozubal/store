cc14368be46ca062967337dd165f59c54d02e734
pawelkozubal <31888831+pawelkozubal@users.noreply.github.com>
2025-05-16 22:59:12 +0100

    another shot at this

--- components/home/Hero.tsx
+++ components/home/Hero.tsx
@@ -6,10 +6,10 @@ function Hero() {
   return (
     <section className='grid grid-cols-1 lg:grid-cols-2 gap-24 items-center'>
       <div>
-        <h1 className='max-w-2xl font-bold text-4xl tracking-tight sm:text-6xl text-primary-foreground'>
+        <h1 className='max-w-2xl font-bold text-4xl tracking-tight sm:text-6xl text-primary'>
           Heaven & Hue
         </h1>
-        <h3 className='max-w-2xl font-semibold text-4xl tracking-tight sm:text-6xl'>
+        <h3 className='mt-8 max-w-2xl font-semibold text-2xl tracking-tight sm:text-3xl'>
           Where Comfort Meets Character
         </h3>
         <p className='mt-8 max-w-xl text-lg leading-8 text-muted-foreground'>
